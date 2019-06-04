106aaf6858644136bd695c0037fc9b4dc48ac3a6
Numan Can İpekçi <n.canipekci@gmail.com>
2019-06-04 15:36:02 +0300

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def plot(generation, allBestFitness, bestGenome, cityLoc):
     plt.subplot(2, 1, 1)
-    plt.text((generation / 2) - 0.5, allBestFitness[0] + 1.2, "Generation: {0} Best Fitness: {1}".format(
+    plt.text((generation / 2) - 0.5, allBestFitness[0] + 10, "Generation: {0} Best Fitness: {1}".format(
         generation, bestGenome.fitness), ha='center', va='bottom')
     plt.plot(range(0, generation), allBestFitness, c="green")
     plt.subplot(2, 1, 2)
