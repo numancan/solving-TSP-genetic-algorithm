440da0e55616dcf8d78ac2e3f210a7a9e68907ff
Numan Can İpekçi <n.canipekci@gmail.com>
2019-06-04 14:13:47 +0300

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -8,18 +8,20 @@ def plot(generation, allBestFitness, bestGenome, cityLoc):
     plt.plot(range(0, generation), allBestFitness, c="green")
     plt.subplot(2, 1, 2)
 
-    xx = []
-    yy = []
     startPoint = None
     for x, y in cityLoc:
         if startPoint is None:
             startPoint = cityLoc[0]
             plt.scatter(startPoint[0], startPoint[1], c="green", marker=">")
+            plt.annotate("Origin", (x + 2, y - 4))
         else:
             plt.scatter(x, y, c="black")
 
-    for i in bestGenome.chromosomes:
-        xx.append(cityLoc[i][0])
-        yy.append(cityLoc[i][1])
+    xx = [cityLoc[i][0] for i in bestGenome.chromosomes]
+    yy = [cityLoc[i][1] for i in bestGenome.chromosomes]
+
+    for x, y in zip(xx, yy):
+        plt.text(x + 2, y - 2, str(yy.index(y)), color="green", fontsize=10)
+
     plt.plot(xx, yy, color="red", linewidth=1.75, linestyle="-")
     plt.show()
